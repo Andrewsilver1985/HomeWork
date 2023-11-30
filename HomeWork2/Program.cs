51b30de04204a58196011b3409365fd7ba867848
Andrew <AndrewSilver@mail.ru>
2023-11-30 19:05:16 +0300

    Вторая задача готова

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rnd = new Random();
+int sise = 10;
+int res;
+int[] array = new int[10];
+for (int i = 0; i < sise; i++)
+{
+    array[i] = rnd.Next(1, 100);
+    System.Console.Write(array[i]+"\t");
+
+
+}
+res =Quantity(array);
+System.Console.WriteLine($"количество четных чисел=>  {res}   ");
+
+static int Quantity(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if ((array[i]%2)==0)
+            count++;
+    }
+    return count;
+
+
+}
