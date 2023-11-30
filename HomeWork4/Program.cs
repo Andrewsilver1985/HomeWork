62e5af7c8bb76e929b00a66b189f7ad28a6f4713
Andrew <AndrewSilver@mail.ru>
2023-11-30 20:49:27 +0300

    Задачка со звездой

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,33 @@
+﻿Random rnd = new Random();
+int value = rnd.Next(1, 1000000);
+int sise = 0;
+Console.WriteLine(value);
+int value1 = value;
+int didgital;
+//int count=0;
+while (value > 0)
+{
+
+    value = value / 10;
+    sise++;
+
+
+}
+System.Console.WriteLine(sise);
+
+int[] array = new int[sise];
+
+for (int i = 0; i < sise; i++)
+{
+    didgital = value1 % 10;
+    value1 = value1 / 10;
+
+    array[i] = didgital;
+    System.Console.Write(array[i] + "  ");
+
+}
+
+
+
+
+
