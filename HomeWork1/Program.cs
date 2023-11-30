08b46a9d15ba85104aebfd6df78c8fea27a6624c
Andrew <AndrewSilver@mail.ru>
2023-11-30 18:52:43 +0300

    Первая задача решена верно

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿Random rnd = new Random();
+int sise = 10;
+int res;
+int[] array = new int[10];
+for (int i = 0; i < sise; i++)
+{
+    array[i] = rnd.Next(1, 100);
+    System.Console.Write(array[i]+"\t");
+
+
+}
+res =Quantity(array);
+System.Console.WriteLine($"количество элементов в отрезке [20,90]=>  {res}   ");
+
+static int Quantity(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 90)
+            count++;
+    }
+    return count;
+
+
+}
